028eaf2270f022223bcce5b9870180ecd4a6cab5
alaninspace <alaninspace@gmail.com>
2026-06-13 09:42:12 +0800

    feat: add favicon and update base template to include it

--- src/docs_web/main.py
+++ src/docs_web/main.py
@@ -231,7 +231,7 @@ def read_doc(request: Request, page_name: str) -> HTMLResponse:
 def main() -> None:
     """Start uvicorn dev server."""
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("docs_web.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("docs_web.main:app", host="127.0.0.1", port=port, reload=True)
 
 
 if __name__ == "__main__":
